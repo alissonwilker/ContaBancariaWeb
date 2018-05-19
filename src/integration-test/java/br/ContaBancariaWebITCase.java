7aacff45233b9a0b610a2daa3832eabee587c206
Alisson Wilker <alissonwilker@gmail.com>
2018-05-19 01:28:14 -0300

    Melhorando codigo

--- src/integration-test/java/br/ContaBancariaWebITCase.java
+++ src/integration-test/java/br/ContaBancariaWebITCase.java
@@ -1,6 +1,7 @@
 package br;
 
 import java.io.File;
+import java.util.function.Function;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -11,19 +12,28 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 @RunWith(Arquillian.class)
 public class ContaBancariaWebITCase {
-    protected static final String TEST_APP_CONTEXT_ROOT = "ContaBancariaWeb";
+    private static final int WAIT_TIMEOUT = 10;
+    private static final String FORM_TAG = "form";
+    private static final String APP_CONTEXT_ROOT = "ContaBancariaWeb";
+    private static final String APP_HOME_URL = "http://localhost:8080/" + APP_CONTEXT_ROOT;
+    private static final String ID_RESPOSTA = "resposta";
 
     @Deployment
     public static Archive<?> criarWebArchiveTeste() {
-        WebArchive webArchiveTeste = ShrinkWrap.create(WebArchive.class, TEST_APP_CONTEXT_ROOT + ".war");
+        WebArchive webArchiveTeste = ShrinkWrap.create(WebArchive.class, APP_CONTEXT_ROOT + ".war");
 
         webArchiveTeste.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
             .importDirectory("src/main/webapp").as(GenericArchive.class), "/", Filters.includeAll());
@@ -38,48 +48,43 @@ public class ContaBancariaWebITCase {
         return webArchiveTeste;
     }
 
-    @Test
-    public void should_create_greeting() {
+    private WebDriver driver;
+
+    @Before
+    public void beforeTest() {
         System.setProperty("webdriver.gecko.driver", "/tmp/geckodriver/geckodriver");
-        
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.addArguments("-headless");
 
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
-        WebDriver driver = new FirefoxDriver(firefoxOptions);
+        FirefoxOptions firefoxOptions = new FirefoxOptions().addArguments("-headless");
+        driver = new FirefoxDriver(firefoxOptions);
+    }
 
-        // And now use this to visit Google
-        driver.get("http://localhost:8080/ContaBancariaWeb/");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
-
-        // Find the text input element by its name
-        // WebElement element = driver.findElement(By.name("q"));
-
-        // Enter something to search for
-        // element.sendKeys("Cheese!");
-
-        // Now submit the form. WebDriver will find the form for us from the element
-        // element.submit();
-
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        // (new WebDriverWait(driver, 10)).until(new Function<WebDriver, Boolean>() {
-        // public Boolean apply(WebDriver d) {
-        // return d.getTitle().toLowerCase().startsWith("cheese!");
-        // }
-        // });
-
-        // Should see: "cheese! - Google Search"
-        // System.out.println("Page title is: " + driver.getTitle());
-
-        // Close the browser
+    @After
+    public void afterTest() {
         driver.quit();
+    }
+
+    @Test
+    public void testRecuperarNomeCliente() {
+        final String NOME_CLIENTE = "Carlos";
+
+        driver.get(APP_HOME_URL);
+        driver.findElement(By.name("nomeCliente")).sendKeys(NOME_CLIENTE);
+        driver.findElement(By.id("tipoClientePessoaFisica")).click();
+        driver.findElement(By.name("cpfCnpj")).sendKeys("053.749...");
+        driver.findElement(By.id("operacaoRecuperarNomeCliente")).click();
+        driver.findElement(By.tagName(FORM_TAG)).submit();
+
+        aguardarResposta(ID_RESPOSTA);
+
+        Assert.assertEquals(NOME_CLIENTE, driver.findElement(By.id(ID_RESPOSTA)).getText());
+    }
+
+    private void aguardarResposta(final String ID_RESPOSTA) {
+        (new WebDriverWait(driver, WAIT_TIMEOUT)).until(new Function<WebDriver, Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return driver.findElement(By.id(ID_RESPOSTA)).isDisplayed();
+            }
+        });
     }
 
 }
