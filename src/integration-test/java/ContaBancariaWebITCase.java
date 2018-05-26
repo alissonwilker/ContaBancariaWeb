92a1aa1af0f37e91b98feac229dc5fcb4255fef7
Alisson Wilker <alissonwilker@gmail.com>
2018-05-26 17:15:32 -0300

    Configurando para funcionar no Eclipse o teste de integracao.

--- src/integration-test/java/ContaBancariaWebITCase.java
+++ src/integration-test/java/ContaBancariaWebITCase.java
@@ -1,8 +1,84 @@
 
-import org.junit.Test;
-import org.openqa.selenium.By;
+import java.io.File;
+import java.util.function.Function;
 
-public class ContaBancariaWebITCase extends AbstractIntegrationTest {
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.Filters;
+import org.jboss.shrinkwrap.api.GenericArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@RunWith(Arquillian.class)
+public class ContaBancariaWebITCase {
+    private static final String FORM_TAG = "form";
+    private static final String APP_CONTEXT_ROOT = "ContaBancariaWeb";
+    private static final String ID_RESPOSTA = "resposta";
+    private static final int WAIT_TIMEOUT = 10;
+
+    protected static final String APP_HOME_URL = "http://localhost:8080/" + APP_CONTEXT_ROOT;
+
+    protected WebDriver driver;
+
+    @Deployment
+    public static Archive<?> criarWebArchiveTeste() {
+        WebArchive webArchiveTeste = ShrinkWrap.create(WebArchive.class, APP_CONTEXT_ROOT + ".war");
+
+        webArchiveTeste.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
+            .importDirectory("src/main/webapp").as(GenericArchive.class), "/", Filters.includeAll());
+
+        File[] bibliotecas = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeAndTestDependencies().resolve()
+            .withTransitivity().asFile();
+        webArchiveTeste.addAsLibraries(bibliotecas).addDefaultPackage();
+
+        webArchiveTeste.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
+            .importDirectory("src/main/resources").as(GenericArchive.class), "/WEB-INF/classes", Filters.includeAll());
+
+        return webArchiveTeste;
+    }
+    
+    @Before
+    public void beforeTest() {
+        System.setProperty("webdriver.gecko.driver", "/tmp/geckodriver/geckodriver");
+
+        FirefoxOptions firefoxOptions = new FirefoxOptions().addArguments("-headless");
+        driver = new FirefoxDriver(firefoxOptions);
+    }
+
+    @After
+    public void afterTest() {
+        driver.quit();
+    }
+
+    private void aguardarResposta(final String ID_RESPOSTA) {
+        (new WebDriverWait(driver, WAIT_TIMEOUT)).until(new Function<WebDriver, Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return driver.findElement(By.id(ID_RESPOSTA)).isDisplayed();
+            }
+        });
+    }
+    
+    protected void submeterEValidar(final String RESPOSTA_ESPERADA) {
+        driver.findElement(By.tagName(FORM_TAG)).submit();
+
+        aguardarResposta(ID_RESPOSTA);
+
+        Assert.assertEquals(RESPOSTA_ESPERADA, driver.findElement(By.id(ID_RESPOSTA)).getText());
+    }
+
     private static final String VALOR_DEPOSITO_TAG = "valorDeposito";
     private static final String VALOR_SAQUE_TAG = "valorSaque";
     private static final String VALOR_0 = "0";
