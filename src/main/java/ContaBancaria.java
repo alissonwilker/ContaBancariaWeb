1d88316808c99b69b85d840102d8c7c8c6f059ad
Alisson Wilker <alissonwilker@gmail.com>
2018-06-18 22:05:55 -0300

    Atualizando ContaBancaria.

--- src/main/java/ContaBancaria.java
+++ src/main/java/ContaBancaria.java
@@ -1,67 +1,89 @@
-
 /**
- * Classe que representa uma conta bancária.
+ * Classe que representa uma conta bancaria.
  *
  */
 public class ContaBancaria {
     /**
-     * Saldo da conta bancária.
+     * Contador de instancias da classe ContaBancaria.
+     */
+    private static int contador;
+
+    /**
+     * Identificador da conta. Representa o numero da instancia de ContaBancaria.
+     */
+    private int id;
+
+    /**
+     * Saldo da conta bancaria.
      */
     private int saldo = 0;
 
     /**
-     * O cliente dono da conta bancária.
+     * O cliente dono da conta bancaria.
      */
     private Cliente cliente;
 
     /**
-     * Construtor que recebe o cliente dono da nova conta bancária. 
+     * Construtor que recebe o cliente dono da nova conta bancaria. Tambem configura o id da conta de
+     * acordo com o contador de instancias, que eh incrementado por este construtor.
      * 
      * @param cliente
-     *            o cliente dono da conta bancária.
+     *            o cliente dono da conta bancaria.
      */
     public ContaBancaria(Cliente cliente) {
+        this.id = ++contador;
         this.cliente = cliente;
     }
 
     /**
-     * Recupera o cliente dono da conta bancária.
+     * Recupera o identificador da conta.
      * 
-     * @return o cliente dono da conta bancária.
+     * @return o id da conta.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Recupera o cliente dono da conta bancaria.
+     * 
+     * @return o cliente dono da conta bancaria.
      */
     public Cliente getCliente() {
         return this.cliente;
     }
 
     /**
-     * Recupera o saldo da conta bancária.
+     * Recupera o saldo da conta bancaria.
      * 
-     * @return o saldo da conta bancária.
+     * @return o saldo da conta bancaria.
      */
     public int getSaldo() {
         return saldo;
     }
 
     /**
-     * Deposita um valor na conta bancária, desde que esse valor seja positivo.
+     * Deposita um valor na conta bancaria, desde que esse valor seja positivo.
      * 
      * @param valor
-     *            o valor a ser depositado na conta bancária. O valor a ser depositado deve ser
+     *            o valor a ser depositado na conta bancaria. O valor a ser depositado deve ser
      *            positivo.
+     * @return o novo saldo da conta bancaria, após o deposito.
      */
-    public void depositar(int valor) {
+    public int depositar(int valor) {
         if (valor > 0) {
             saldo = saldo + valor;
         }
+        return saldo;
     }
 
     /**
-     * Saca um valor da conta bancária, se esse valor for positivo e menor ou igual ao saldo.
+     * Saca um valor da conta bancaria, se esse valor for positivo e menor ou igual ao saldo.
      * 
      * @param valor
-     *            o valor a ser sacado da conta bancária. O valor a ser sacado deve ser positivo e menor
-     *            ou igual ao saldo da conta bancária.
-     * @return o novo saldo da conta bancária, após o saque.
+     *            o valor a ser sacado da conta bancaria. O valor a ser sacado deve ser positivo e menor
+     *            ou igual ao saldo da conta bancaria.
+     * @return o novo saldo da conta bancaria, apos o saque.
      */
     public int sacar(int valor) {
         if (valor > 0 && valor <= saldo) {
